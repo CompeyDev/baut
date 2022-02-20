0462fa010dc031e134bbb89458bde7b4404c9b50
Milo <50248166+Milo123459@users.noreply.github.com>
2022-02-20 14:36:22 +0000

    comment

--- src/events/message/create.ts
+++ src/events/message/create.ts
@@ -26,42 +26,42 @@ export default new Event(
 		}
 
 		// SALVAGE_LIKES_TO_SCREAM
-		switch (message.channel.id) {
-			case channels.wins:
-				message.startThread({
-					name: 'Win Thread',
-					autoArchiveDuration: 1440,
-					reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
-				});
-				break;
-			case channels.help:
-				message.startThread({
-					name: 'Help Thread',
-					autoArchiveDuration: 1440,
-					reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
-				});
-				break;
-			case channels.resources:
-				message.startThread({
-					name: 'Resource Thread',
-					autoArchiveDuration: 1440,
-					reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
-				});
-				break;
-			case channels.jobs:
-				message.startThread({
-					name: 'Job Thread',
-					autoArchiveDuration: 1440,
-					reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
-				});
-				break;
-			case channels.showcase:
-				message.startThread({
-					name: 'Showcase Thread',
-					autoArchiveDuration: 1440,
-					reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
-				});
-				break;
-		}
+		// switch (message.channel.id) {
+		// 	case channels.wins:
+		// 		message.startThread({
+		// 			name: 'Win Thread',
+		// 			autoArchiveDuration: 1440,
+		// 			reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
+		// 		});
+		// 		break;
+		// 	case channels.help:
+		// 		message.startThread({
+		// 			name: 'Help Thread',
+		// 			autoArchiveDuration: 1440,
+		// 			reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
+		// 		});
+		// 		break;
+		// 	case channels.resources:
+		// 		message.startThread({
+		// 			name: 'Resource Thread',
+		// 			autoArchiveDuration: 1440,
+		// 			reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
+		// 		});
+		// 		break;
+		// 	case channels.jobs:
+		// 		message.startThread({
+		// 			name: 'Job Thread',
+		// 			autoArchiveDuration: 1440,
+		// 			reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
+		// 		});
+		// 		break;
+		// 	case channels.showcase:
+		// 		message.startThread({
+		// 			name: 'Showcase Thread',
+		// 			autoArchiveDuration: 1440,
+		// 			reason: `[Baut AutoThread] Thread created for ${message.author.tag}`,
+		// 		});
+		// 		break;
+		// }
 	}
 );
